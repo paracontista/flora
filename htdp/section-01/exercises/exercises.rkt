00ab47b5013bf571ad2873cf41529b9ba295722f
paracontista <me@paracontista.com>
2026-05-22 21:11:22 +0200

    Update exercises.rkt

--- htdp/section-01/exercises/exercises.rkt
+++ htdp/section-01/exercises/exercises.rkt
@@ -4,12 +4,28 @@
 
 (define x 3)
 (define y 4)
+(define prefix "hello")
+(define suffix "world")
+(define underscore "_")
+(define str "helloworld")
+(define i 5)
 
 ;; distance : (Int, Int) -> Int
-;; relative to origin (0,0) in the cartesian plane.
+;; function which calculates distance of a point (x,y) relative to origin (0,0) in the cartesian plane.
 (define (distance x y)
   (sqrt (+ (expt x 2) (expt y 2))))
 
-;; (distance 1 0)
-;; (distance 0 1)
-;; (distance 0 0)
+;; (distance 1 0) (distance 0 1) ...
+
+;; add-middle-char : String -> String -> String
+;; function which appends an underscore between two strings.
+(define (add-middle-char s s2)
+  (string-append s underscore s2))
+
+;; (add-middle-char prefix suffix)
+
+;; add-char-at-index : String -> String -> Int -> String
+;; function which appends an underscore at a given index inside a String.
+(define (add-char-at-index s c idx) '())
+
+;; (add-char-at-index str underscore i)
