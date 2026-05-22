ecbb12d97ebf92ad40a9df3908c51309398ffcc8
paracontista <me@paracontista.com>
2026-05-22 22:28:56 +0200

    Update exercises.rkt (Arithmetic of Strings)

--- htdp/section-01/exercises/exercises.rkt
+++ htdp/section-01/exercises/exercises.rkt
@@ -1,6 +1,4 @@
-(module exercises racket)
-
-#lang racket
+#lang htdp/bsl
 
 (define x 3)
 (define y 4)
@@ -10,23 +8,31 @@
 (define str "helloworld")
 (define i 5)
 
+;; NOTE: In htdp/bsl Char is renamed to 1String.
+
 ;; distance : (Int, Int) -> Int
 ;; function which calculates distance of a point (x,y) relative to origin (0,0) in the cartesian plane.
 (define (distance x y)
   (sqrt (+ (expt x 2) (expt y 2))))
 
-;; add-middle-char : String -> String -> String
+;; add-middle-char : String -> 1String -> String
 ;; function which appends an underscore between two strings.
 (define (add-middle-char s s2)
   (string-append s underscore s2))
 
-;; add-char-at-index : String -> Char -> Int -> String
+;; add-char-at-index : String -> 1String -> Int -> String
 ;; function which appends a Char at a given index inside a String.
 (define (add-char-at-index s c idx)
   (string-append (substring s 0 idx) c (substring s idx)))
 
-#|
-(distance 1 0) (distance 0 1) ...
-(add-middle-char prefix suffix)
-(add-char-at-index str underscore i)
-#|
+;; delete-char-at-index : String -> Int -> String
+;; function which *removes* a character at given index inside a String.
+(define (delete-char-at-index s idx)
+  (string-append (substring s 0 (- idx 1)) (substring s idx)))
+
+;; (distance x y)
+;; (distance 1 0)
+;; (distance 0 1)
+;; (add-middle-char prefix suffix)
+;; (add-char-at-index str underscore i)
+;; (delete-char-at-index str i)
